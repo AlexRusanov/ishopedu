24596d2b07cb7cb956f2b149a48ee78a16ab4c36
AlexRusanov <alex.dalamar@me.com>
2017-08-23 19:52:55 +0300

    HW12

--- src/test/java/edu/bionic/dao/OrderDaoTest.java
+++ src/test/java/edu/bionic/dao/OrderDaoTest.java
@@ -1,6 +1,8 @@
 package edu.bionic.dao;
 
 import edu.bionic.domain.Order;
+import edu.bionic.testdata.OrderFactory;
+import edu.bionic.testdata.UserFactory;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 
 import static edu.bionic.testdata.OrderFactory.getAllOrders;
-import static edu.bionic.testdata.OrderFactory.getNewOrder;
+import static edu.bionic.testdata.OrderFactory.getAllOrdersForUser;
+import static edu.bionic.testdata.OrderFactory.getNewOrderForUser;
 
 public class OrderDaoTest extends BaseDaoTest {
 
@@ -24,15 +27,23 @@ public class OrderDaoTest extends BaseDaoTest {
     }
 
     @Test
+    public void getAllByUser() throws Exception {
+        List<Order> expected = getAllOrdersForUser();
+        List<Order> actual = orderDao.getAllByUser(UserFactory.getUser().getId());
+
+        Assert.assertEquals(expected.toString(), actual.toString());
+    }
+
+    @Test
     public void save() throws Exception {
-        Order newOrder = getNewOrder();
+        Order newOrder = getNewOrderForUser();
         Order savedOrder = orderDao.save(newOrder);
         newOrder.setId(savedOrder.getId());
 
-        List<Order> expected = getAllOrders();
+        List<Order> expected = OrderFactory.getAllOrdersForUser();
         expected.add(newOrder);
 
-        List<Order> actual = orderDao.getAll();
+        List<Order> actual = orderDao.getAllByUser(UserFactory.getUser().getId());
 
         Assert.assertEquals(expected.toString(), actual.toString());
     }
--- src/test/java/edu/bionic/testdata/OrderFactory.java
+++ src/test/java/edu/bionic/testdata/OrderFactory.java
@@ -2,6 +2,7 @@ package edu.bionic.testdata;
 
 import com.google.common.collect.Lists;
 import edu.bionic.domain.Order;
+import edu.bionic.domain.User;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -19,7 +20,8 @@ public class OrderFactory {
                 "Олег",
                 "oleg@gmail.com",
                 "095664356273",
-                "ул. Дорогожицкая, 1"
+                "ул. Дорогожицкая, 1",
+                UserFactory.getUser()
         );
     }
 
@@ -32,7 +34,8 @@ public class OrderFactory {
                 "Аня",
                 "anya@gmail.com",
                 "095664357685",
-                "ул. Артема, 30"
+                "ул. Артема, 30",
+                null
         );
     }
 
@@ -40,7 +43,11 @@ public class OrderFactory {
         return Lists.newArrayList(getOrder1(), getOrder2());
     }
 
-    public static Order getNewOrder() {
+    public static List<Order> getAllOrdersForUser() {
+        return Lists.newArrayList(getOrder1());
+    }
+
+    public static Order getNewOrderForUser() {
         return new Order(
                 null,
                 LocalDateTime.of(2017, Month.AUGUST, 11, 11, 11),
@@ -49,7 +56,8 @@ public class OrderFactory {
                 "Игорь",
                 "ihor@gmail.com",
                 "0956644347685",
-                "ул. Артема, 31"
+                "ул. Артема, 31",
+                UserFactory.getUser()
         );
     }
 
